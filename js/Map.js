16c30d40e4d46bb68e760d3bd0fbc3f7d5f04996
Bryan Lin <bryan.t.lin1@gmail.com>
2011-12-21 00:15:31 -0500

    added map for geoiq api. still need to figure out why events do not work

--- js/Map.js
+++ js/Map.js
@@ -2,7 +2,26 @@
  * 
  */
 
+/** namespace **/
+var com = com || {};
+com.linbr = com.linbr || {};
+com.linbr.Map = com.linbr.Map || {};
 
-Map.prototype.Map = function() {
-	 maker_map_127297 = new F1.Maker.Map({map_id: "127297", dom_id: "maker_map_127297"});
+
+com.linbr.Map = function (mapId, domId) {
+	this.geoIQMap = new F1.Maker.Map({ map_id: mapId,
+										dom_id: domId,
+										uiLayers : true
+									});
+	
+	this.geoIQMap.setCallback('onMapLoaded', this.showEvent('onMapLoaded','wooo'));
+	
+};
+
+com.linbr.Map.prototype.initializeMapComponents = function() {	
+
+};
+
+com.linbr.Map.prototype.showEvent = function (eventName, message) {
+	$("callback_results").innerHTML += eventName + " = " + message + "<br/>" ;
 };
